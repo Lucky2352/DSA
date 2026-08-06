560d298e718c5488c2b1588a408da9bb71f541bf
Lucky2352 <luckyyadav1586@gmail.com>
2026-08-06 17:12:12 +0530

    Time: 1 ms (86.72%) | Memory: 44.3 MB (39.51%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,17 @@
 class Solution {
-    public List<List<Integer>> list2 = new ArrayList<>();
-  public void recursion2(List<Integer> vals, int arr[],int i){
-      if(i == arr.length){
-        list2.add(new ArrayList<>(vals));
-        return;
-      }
-      vals.add(arr[i]);
-      recursion2(vals,arr,i + 1);
-      vals.remove(vals.size() - 1);
-      recursion2(vals,arr,i+1);
-  }
+    public static List<List<Integer>> recursion(int nums[],int i,List<Integer> list,List<List<Integer>> ans){
+        if(i == nums.length){
+            ans.add(new ArrayList<>(list));
+            return ans;
+        }
+        list.add(nums[i]);
+        List<List<Integer>> left = recursion(nums,i + 1,list,ans);
+        list.remove(list.size() - 1);
+        List<List<Integer>> right = recursion(nums,i + 1,list,ans);
+        return ans;
+    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> vals = new ArrayList<>();
-        recursion2(vals,nums,0);
-        return list2;
+        List<List<Integer>> list = new ArrayList<>();
+        return recursion(nums,0,new ArrayList<>(),list);
     }
 }
