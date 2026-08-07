608fe9366b6a4ef4bef37f5ef008774ba60cd3eb
Lucky2352 <luckyyadav1586@gmail.com>
2026-08-07 17:49:24 +0530

    Time: 3 ms (29.39%) | Memory: 43.4 MB (64.26%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,18 +1,27 @@
 class Solution {
-    public static void recursion(String p,String up,String[] arr,List<String> list){
-        if(up.isEmpty()){
+    public static void recursion(String p, String up, String[] arr, List<String> list) {
+        if (up.isEmpty()) {
             list.add(p);
             return;
         }
         String psb = arr[up.charAt(0) - '0'];
-        for(int i = 0;i<psb.length();i++){
-            recursion(p + psb.charAt(i),up.substring(1),arr,list);
+        for (int i = 0; i < psb.length(); i++) {
+            recursion(
+                p + psb.charAt(i),
+                up.substring(1),
+                arr,
+                list
+            );
         }
     }
     public List<String> letterCombinations(String d) {
-        String arr[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if (d.length() == 0) return new ArrayList<>();
+        String[] arr = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
         List<String> ans = new ArrayList<>();
-        recursion("",d,arr,ans);
+        recursion("", d, arr, ans);
         return ans;
     }
 }
