d493a4b57c3f74c99e6244962951b40dde7da72e
Lucky2352 <luckyyadav1586@gmail.com>
2026-08-07 17:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (50.18%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,27 +1,23 @@
 class Solution {
-    public static void recursion(String p, String up, String[] arr, List<String> list) {
-        if (up.isEmpty()) {
-            list.add(p);
+    public static void recursion(String digits,int index,StringBuilder current,String[] arr,List<String> list) {
+        if (index == digits.length()) {
+            list.add(current.toString());
             return;
         }
-        String psb = arr[up.charAt(0) - '0'];
-        for (int i = 0; i < psb.length(); i++) {
-            recursion(
-                p + psb.charAt(i),
-                up.substring(1),
-                arr,
-                list
-            );
+        String letters = arr[digits.charAt(index) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            current.append(letters.charAt(i));
+            recursion(digits, index + 1, current, arr, list);
+            current.deleteCharAt(current.length() - 1);
         }
     }
-    public List<String> letterCombinations(String d) {
-        if (d.length() == 0) return new ArrayList<>();
-        String[] arr = {
-            "", "", "abc", "def", "ghi",
-            "jkl", "mno", "pqrs", "tuv", "wxyz"
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0)
+            return new ArrayList<>();
+        String[] arr = {"", "", "abc", "def", "ghi","jkl", "mno", "pqrs", "tuv", "wxyz"
         };
         List<String> ans = new ArrayList<>();
-        recursion("", d, arr, ans);
+        recursion(digits, 0, new StringBuilder(), arr, ans);
         return ans;
     }
 }
