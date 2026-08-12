9a38563381d93c8bafa00ebefd3c68afd139d99d
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-12 11:04:05 +0530

    Time: 75 ms (26.02%) | Memory: 88.9 MB (64.18%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,31 +1,26 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
         int i = 0;
-        int j = 0;
         int maxi = 0;
-        while(j < nums.length){
-            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-            max = Math.max(max,map.get(nums[j]));
-            if(max > k){
-                int conflict = nums[j];
 
+        for (int j = 0; j < nums.length; j++) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
 
+            while (map.get(nums[j]) > k) {
+                map.put(nums[i], map.get(nums[i]) - 1);
 
-                while(map.get(nums[j]) > k){
-                    map.put(nums[i],map.getOrDefault(nums[i],0)-1);
-                    if(map.get(nums[i]) == 0){
-                        map.remove(nums[i]);
-                    }
-                    i++;
+                if (map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
                 }
+
+                i++;
             }
-                max = map.get(nums[j]);
-                j++;
-            maxi = Math.max(maxi,j-i);
-            
+
+            maxi = Math.max(maxi, j - i + 1);
         }
+
         return maxi;
     }
 }
