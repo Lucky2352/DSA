1a3efad11e41b7ec084ace44ab24132564dc8ad0
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-16 20:57:17 +0530

    Time: 3 ms (49.25%) | Memory: 44.9 MB (44.00%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -2,8 +2,6 @@ class Solution {
     public static boolean isValid(String str) {
         str = str.substring(0, str.length() - 1);
         String[] parts = str.split("\\.");
-        if (parts.length != 4)
-            return false;
         for (String part : parts) {
             if (part.length() > 1 && part.charAt(0) == '0')
                 return false;
@@ -13,34 +11,42 @@ class Solution {
         }
         return true;
     }
-    public static void recursion(StringBuilder sb, String s,List<String> ans, int i) {
+    public static void recursion(StringBuilder sb, String s,List<String> ans, int i, int parts) {
+        if (parts > 4)
+            return;
         if (i == s.length()) {
-            if (isValid(sb.toString())) {
+            if (parts == 4) {
                 String possible = sb.toString();
-                ans.add(possible.substring(0, possible.length() - 1));
+                if (isValid(possible)) {
+                    ans.add(possible.substring(0, possible.length() - 1));
+                }
             }
             return;
         }
         sb.append(s.charAt(i));
         sb.append('.');
-        recursion(sb, s, ans, i + 1);
+        recursion(sb, s, ans, i + 1, parts + 1);
         sb.delete(sb.length() - 2, sb.length());
+
+
         if (i + 2 <= s.length()) {
             sb.append(s.substring(i, i + 2));
             sb.append('.');
-            recursion(sb, s, ans, i + 2);
+            recursion(sb, s, ans, i + 2, parts + 1);
             sb.delete(sb.length() - 3, sb.length());
         }
+
+
         if (i + 3 <= s.length()) {
             sb.append(s.substring(i, i + 3));
             sb.append('.');
-            recursion(sb, s, ans, i + 3);
+            recursion(sb, s, ans, i + 3, parts + 1);
             sb.delete(sb.length() - 4, sb.length());
         }
     }
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
-        recursion(new StringBuilder(), s, ans, 0);
+        recursion(new StringBuilder(), s, ans, 0, 0);
         return ans;
     }
 }
