7e1e251f03bd19aa1339f98dd29da1dade475957
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-18 20:14:48 +0530

    Time: 325 ms (5.45%) | Memory: 75.7 MB (73.39%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,50 +1,49 @@
 class Solution {
-    public static void findNSE(int[] arr, List<Integer> list) {
+    public static void NSE(int[] arr,List<Integer> list){
         Stack<Integer> st = new Stack<>();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+        list.add(arr.length);
+        st.push(arr.length - 1);
+        for(int i = arr.length - 2;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
                 st.pop();
             }
-            if (st.isEmpty()) {
+            if(st.isEmpty()){
                 list.add(arr.length);
-            } else {
+            }else{ 
                 list.add(st.peek());
             }
             st.push(i);
         }
         Collections.reverse(list);
     }
-    public static void findPSEE(int[] arr, List<Integer> list) {
+    public static void PSEE(int[] arr,List<Integer> list){
         Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < arr.length; i++) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+        list.add(-1);
+        st.push(0);
+        for(int i = 1;i<arr.length;i++){
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
                 st.pop();
             }
-            if (st.isEmpty()) {
+            if(st.isEmpty()){
                 list.add(-1);
-            } else {
+            }else{ 
                 list.add(st.peek());
             }
             st.push(i);
         }
     }
-
     public int sumSubarrayMins(int[] arr) {
         long mod = 1000000007;
-        long total = 0;
-
-        List<Integer> NSE = new ArrayList<>();
-        List<Integer> PSEE = new ArrayList<>();
-
-        findNSE(arr, NSE);
-        findPSEE(arr, PSEE);
-
-        for (int i = 0; i < arr.length; i++) {
-            long left = i - PSEE.get(i);
-            long right = NSE.get(i) - i;
-
-            total = (total + (long) arr[i] * left * right) % mod;
+        long ans = 0;
+        List<Integer> nse = new ArrayList<>();
+        List<Integer> psee = new ArrayList<>();
+        NSE(arr,nse);
+        PSEE(arr,psee);
+        for(int i = 0;i<arr.length;i++){
+            long cur = i - psee.get(i);
+            long cur2 = nse.get(i) - i;
+            ans = (int)(ans + ((long)arr[i] * (cur * cur2) % mod)) % mod;
         }
-        return (int) total;
+        return (int)ans;
     }
 }
