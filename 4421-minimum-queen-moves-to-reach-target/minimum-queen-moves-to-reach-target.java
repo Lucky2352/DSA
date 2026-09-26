a4b02f21f97cd58d31a449406af390071ad728a5
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-26 22:20:03 +0530

    Time: 1 ms (95.47%) | Memory: 44 MB (85.02%) - LeetSync

---
+++ 4421-minimum-queen-moves-to-reach-target/minimum-queen-moves-to-reach-target.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minQueenMoves(int[] source, int[] target) {
+        int r1 = source[0];
+        int r2 = target[0];
+        int c1 = source[1];
+        int c2 = target[1];
+        if (r1 == r2 && c1 == c2) {
+            return 0;
+        }
+
+        if (r1 == r2 || c1 == c2) {
+            return 1;
+        }
+
+        if (Math.abs(r1 - r2) == Math.abs(c1 - c2)) {
+            return 1;
+        }
+
+        return 2;
+    }
+}
